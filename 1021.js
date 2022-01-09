6a41e6f90bf1b9bd27452e6f9a485a3b6bfeb979
Gustavo Carvalho <gustavo1net@hotmail.com>
2022-01-08 22:17:31 -0300

    1021 - Notas e Moedas

---
+++ 1021.js
@@ -0,0 +1,43 @@
+var input = require('fs').readFileSync('input', 'utf8');
+var lines = input.split('\n');
+
+var valor = parseFloat(lines[0]);
+var n100 = Math.floor(valor / 100);
+valor %= 100;
+var n50 = Math.floor(valor / 50);
+valor %= 50;
+var n20 = Math.floor(valor / 20);
+valor %= 20;
+var n10 = Math.floor(valor / 10);
+valor %= 10;
+var n5 = Math.floor(valor / 5);
+valor %= 5;
+var n2 = Math.floor(valor / 2);
+valor %= 2;
+var n1 = Math.floor(valor);
+valor %= 1;
+var n05 = Math.floor(valor / 0.5);
+valor %= 0.5;
+var n025 = Math.floor(valor / 0.25);
+valor %= 0.25;
+var n010 = Math.floor(valor / 0.1);
+valor %= 0.1;
+var n005 = Math.floor(valor / 0.05);
+valor %= 0.05;
+var n001 = Math.round(valor * 100);
+
+
+console.log("NOTAS:");
+console.log(`${n100} nota(s) de R$ 100.00`);
+console.log(`${n50} nota(s) de R$ 50.00`);
+console.log(`${n20} nota(s) de R$ 20.00`);
+console.log(`${n10} nota(s) de R$ 10.00`);
+console.log(`${n5} nota(s) de R$ 5.00`);
+console.log(`${n2} nota(s) de R$ 2.00`);
+console.log("MOEDAS:");
+console.log(`${n1} moeda(s) de R$ 1.00`);
+console.log(`${n05} moeda(s) de R$ 0.50`);
+console.log(`${n025} moeda(s) de R$ 0.25`);
+console.log(`${n010} moeda(s) de R$ 0.10`);
+console.log(`${n005} moeda(s) de R$ 0.05`);
+console.log(`${n001} moeda(s) de R$ 0.01`);
